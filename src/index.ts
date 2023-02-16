de78081f779cb3369b0bf750a50d718e6f345086
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-02-16 21:28:50 +0100

    Add basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hi Mom!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => res.send('Hello Express!'))
+
+app.get('/api', (req: Request, res: Response) => res.json({ message: 'Hello Express!' }))
+
+app.listen(3000, () => console.log('The app is listening on port ' + 3000))
