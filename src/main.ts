2335f3391ebbb479d9acd5f330e140153df65623
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-02-19 14:46:46 +0100

    Add tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,1 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-
-const app = document.querySelector<HTMLDivElement>('#app');
-
-app!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
