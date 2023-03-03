444f482b4cd6b9920284cd2adfe2ad0367934f42
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-03-03 21:01:43 +0100

    Remove mistake during rebase

--- src/class/interface/CanvasRenderingContext.ts
+++
@@ -1,7 +0,0 @@
-interface CanvasRenderingContext {
-    get ctx(): CanvasRenderingContext2D;
-
-    set ctx(context: CanvasRenderingContext2D);
-}
-
-export default CanvasRenderingContext;
--- src/class/interface/GameBoard.ts
+++
@@ -1,9 +0,0 @@
-import Board from '../components/Board';
-
-interface GameBoard {
-    get board(): Board;
-
-    set board(board: Board);
-}
-
-export default GameBoard;
